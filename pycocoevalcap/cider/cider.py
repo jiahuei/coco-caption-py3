046b9618426a30c96c90ff87536688072aff2e74
jiahuei <tanjiahuei@gmail.com>
2019-12-14 19:15:13 +0800

    cosmetic changes

--- pycocoevalcap/cider/cider.py
+++ pycocoevalcap/cider/cider.py
@@ -10,17 +10,19 @@
 from cider_scorer import CiderScorer
 import pdb
 
+
 class Cider:
     """
     Main Class to compute the CIDEr metric 
 
     """
+    
     def __init__(self, test=None, refs=None, n=4, sigma=6.0):
         # set cider to sum over 1 to 4-grams
         self._n = n
         # set the standard deviation parameter for gaussian penalty
         self._sigma = sigma
-
+    
     def compute_score(self, gts, res):
         """
         Main function to compute CIDEr score
@@ -28,27 +30,27 @@ class Cider:
                 ref_for_image (dict)  : dictionary with key <image> and value <tokenized reference sentence>
         :return: cider (float) : computed CIDEr score for the corpus 
         """
-
-        assert(gts.keys() == res.keys())
+        
+        assert (gts.keys() == res.keys())
         imgIds = gts.keys()
-
+        
         cider_scorer = CiderScorer(n=self._n, sigma=self._sigma)
-
+        
         for id in imgIds:
             hypo = res[id]
             ref = gts[id]
-
+            
             # Sanity check.
-            assert(type(hypo) is list)
-            assert(len(hypo) == 1)
-            assert(type(ref) is list)
-            assert(len(ref) > 0)
-
+            assert (type(hypo) is list)
+            assert (len(hypo) == 1)
+            assert (type(ref) is list)
+            assert (len(ref) > 0)
+            
             cider_scorer += (hypo[0], ref)
-
+        
         (score, scores) = cider_scorer.compute_score()
-
+        
         return score, scores
-
+    
     def method(self):
         return "CIDEr"
