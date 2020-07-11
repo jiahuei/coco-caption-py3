ce8545bd9b08e20c2b1c08ceacceeae32f3c8842
jiahuei <tanjiahuei@gmail.com>
2020-07-11 19:59:57 +0800

    java, docker_run

---
+++ docker/docker_run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker run -it \
+    --gpus all \
+    -v "${SCRIPT_ROOT}/../coco_caption_py2:/coco_caption_py2" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="$DISPLAY" \
+    -u "$(id -u)":"$(id -g)" \
+    --rm jiahuei/python27:java8
